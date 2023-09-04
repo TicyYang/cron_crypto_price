3aaa9128f0ad72f3826e2764f99e70e4db3485ca
Ticy Yang <109358237+TicyYang@users.noreply.github.com>
2023-09-04 20:47:37 +0800

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -5,4 +5,4 @@ CONTAINER_NAME="crypto_ctr"
 
 docker build -t $IMAGE_NAME .
 
-docker run --name $CONTAINER_NAME -it -v ~/datasets:/app/datasets $IMAGE_NAME
+docker run --name $CONTAINER_NAME -v ~/datasets:/app/datasets $IMAGE_NAME
